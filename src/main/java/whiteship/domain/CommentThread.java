9a70fcf5f9f6015a57115cdfeb77e7fdb9839aef
Keesun Baik <whiteship@epril.com>
2013-12-25 15:41:43 +0900

    @ManyToOne @JoinTable

--- src/main/java/whiteship/domain/CommentThread.java
+++ src/main/java/whiteship/domain/CommentThread.java
@@ -23,6 +23,11 @@ public class CommentThread {
     private ThreadState state;
 
     @ManyToOne
+    @JoinTable(
+        name = "comment_thread_pull_request",
+        joinColumns = @JoinColumn(name = "comment_thread_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "pull_request_id", referencedColumnName = "id")
+    )
     private PullRequest pullRequest;
 
     public Long getId() {
